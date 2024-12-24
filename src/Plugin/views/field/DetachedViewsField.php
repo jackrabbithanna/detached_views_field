e2d6456be9dfce21217fcd41a07d333f572def74
Mark Hanna <mark@skvare.com>
2024-12-24 12:39:43 -0700

    initial commit

---
+++ src/Plugin/views/field/DetachedViewsField.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Drupal\detached_views_field\Plugin\views\field;
+
+use Drupal\civicrm_entity\CiviCrmApi;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * @file
+ * Defines Drupal\detached_views_field\Plugin\views\field\DetachedViewsField.
+ */
+
+/**
+ * Field handler to flag the node type.
+ *
+ * @ingroup views_field_handlers
+ * @ViewsField("field_detached_views_field")
+ */
+class DetachedViewsField extends FieldPluginBase {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The CiviCRM API Service
+   * @var \Drupal\civicrm_entity\CiviCrmApi
+   */
+  protected $civicrmApi;
+  /**
+   * Views Cumulative Field constructor.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, CiviCrmApi $civicrm_api) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityTypeManager = $entity_type_manager;
+    $this->civicrmApi = $civicrm_api;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static($configuration, $plugin_id, $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('civicrm_entity.api')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    // Do nothing -- to override the parent query.
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function usesGroupBy() {
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render(ResultRow $values) {
+    $contact = $values->_entity;
+    $contact_id = $contact->get('id')->getValue()[0]['value'];
+    $address_result = $this->civicrmApi->get('Address', [
+      'contact_id' => $contact_id,
+      'is_primary' => TRUE,
+      'sequential' => TRUE,
+    ]);
+    if (!empty($address_result[0]['street_address'])) {
+      return $address_result[0]['street_address'];
+    }
+  }
+}
